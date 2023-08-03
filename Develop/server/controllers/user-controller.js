6771b9600e8bc03743933c7c75699a8987c89ef7
kaleikautakaoka <sachikaleikautakaoka@gmail.com>
2023-08-03 16:26:35 -0600

    query file

--- Develop/server/controllers/user-controller.js
+++ Develop/server/controllers/user-controller.js
@@ -34,9 +34,9 @@ module.exports = {
       return res.status(400).json({ message: "Can't find this user" });
     }
 
-    const correctPw = await user.isCorrectPassword(body.password);
+    const validPwd = await user.isCorrectPassword(body.password);
 
-    if (!correctPw) {
+    if (!validPwd) {
       return res.status(400).json({ message: 'Wrong password!' });
     }
     const token = signToken(user);
