6af32c0bc13072aae7f377720b08efbf15de5d6b
kaleikautakaoka <sachikaleikautakaoka@gmail.com>
2023-08-03 16:49:31 -0600

    connected apollo in app.js

--- Develop/client/src/App.js
+++ Develop/client/src/App.js
@@ -4,8 +4,37 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+//imported apollo packages
+import { AppoloClient, InmemoryCache, ApolloProvider, createHTTpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+//established new link to the graphql server at /graphql
+const httpLink = createHTTpLink({
+  uri: '/graphql'
+});
+
+//established middleware to retrieve token from local storage and set the request headers before making the graphql api request
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      // if token, add to headers
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  };
+});
+
+//established new instance of apollo client
+const client = new AppoloClient({
+  link: authLink.concat(httpLink),
+  cache: new InmemoryCache()
+});
+
+//established apollo provider component to make every request work with apollo client
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
@@ -25,6 +54,7 @@ function App() {
         </Routes>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
