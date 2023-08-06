fa8b7bf0a17b82ab2fe48c3afac58c9f8f98522a
kaleikautakaoka <sachikaleikautakaoka@gmail.com>
2023-08-06 01:30:46 -0600

    push after nvm work around and lights out

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,13 +1,3 @@
-//  `mutations.js`:
-
-// 	 `LOGIN_USER` will execute the `loginUser` mutation set up using Apollo Server.
-
-// 	 `ADD_USER` will execute the `addUser` mutation.
-
-// 	 `SAVE_BOOK` will execute the `saveBook` mutation.
-
-// 	 `REMOVE_BOOK` will execute the `removeBook` mutation.
-
 import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
